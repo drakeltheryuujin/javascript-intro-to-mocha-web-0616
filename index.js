1f0300b403dd407e08aea8cf2a576b2eb42f7642
Kellye Greene <kellye@gust.com>
2016-08-03 13:32:38 -0400

    add functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}.`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
+
+function theTruth() {
+  return true;
+}
